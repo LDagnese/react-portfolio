b5d392431e013d6c92db46514f59839acccf339f
loudagnese <louis.dagnese@gmail.com>
2022-09-25 12:10:19 -0700

    added react-bootstrap and created Nav Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavHeader from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <NavHeader />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Nav = () => {
-    return (
-        <header>
-            Nav bar
-        </header>
-    )
+function NavHeader() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Louis' React Portfolio</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#about">About Me</Nav.Link>
+            <Nav.Link href="#contact">Contact Me</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
-export default Nav;
+export default NavHeader;
