ccfc14fbad4768d98960f46176e664eb237b3e32
loudagnese <louis.dagnese@gmail.com>
2022-09-27 10:03:51 -0700

    added the aboutme component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavHeader from './components/Nav';
 import Footer from './components/Footer';
 import Projects from './components/Project';
+import Aboutme from './components/About Me';
 
 function App() {
   return (
     <div className='d-flex flex-column min-vh-100'>
       <NavHeader />
       <Projects />
+      <Aboutme />
       <Footer />
     </div>
   );
---
+++ src/components/About Me/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Aboutme() {
+    return(
+        <div>
+            This is the about me section!
+        </div>
+    )
+}
+
+export default Aboutme;
